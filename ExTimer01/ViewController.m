c0a4db32fb471dfe5d9a232f700dd4e648bd0ca5
Chae Yungi <l8673901@gmail.com>
2014-01-06 13:21:18 +0900

    타이머 예제입니다!
    타이머 예제입니다!

--- ExTimer01/ViewController.m
+++ ExTimer01/ViewController.m
@@ -9,15 +9,50 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+	NSTimer *timer;
+	BOOL timerWorking;
+}
+
+- (void)tickDown:(NSTimer *)timer {
+	self.datePicker.countDownDuration = self.datePicker.countDownDuration - 60;
+	
+	if(self.datePicker.countDownDuration <= 60) {
+		NSLog(@"Done!");
+		[self stopTimer];
+	}
+}
+
+- (void)stopTimer {
+	[self->timer invalidate];
+	timer = nil;
+	timerWorking = NO;
+}
+
+- (void)startTimer {
+	timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tickDown:) userInfo:nil repeats:YES];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)toggleTimer:(id)sender {
+	NSString *title = timerWorking?@"start":@"stop";
+	[sender setTitle:title forState:UIControlStateNormal];
+	
+	timerWorking = !timerWorking;
+	
+	if(timerWorking) {
+		[self startTimer];
+	} else {
+		[self stopTimer];
+	}
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +62,24 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
